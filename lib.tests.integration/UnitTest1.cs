3566603d63b997044a8def5f837212d052f0cf9b
EDooley <EDooley@goempyrean.com>
2023-05-08 16:27:07 -0500

    added integration test project

---
+++ lib.tests.integration/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace lib.tests.integration;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
